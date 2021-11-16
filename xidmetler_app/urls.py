b42a4788e6102bd313cab58af3624ca6bea0fa6e
Your Name <you@example.com>
2021-11-16 16:39:35 +0400

    Qweqwe

---
+++ xidmetler_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+app_name = 'xidmetler_app'
+
+urlpatterns = [
+    path('xidmetlerimiz/<int:pk>/', views.NewsDetailView.as_view(), name='news-detail')
